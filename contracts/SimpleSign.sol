8db7bfbbd6094dfb5c9ddb2d9541f2c2b4918b46
Igor Artamonov <igor@artamonov.ru>
2016-06-12 15:58:30 +0600

    narrow data size

--- contracts/SimpleSign.sol
+++ contracts/SimpleSign.sol
@@ -2,21 +2,22 @@ contract SimpleSign {
 
     event Created(
         address indexed from,
-        bytes32 id
+        uint256 id
     );
     event Signed(
         address indexed from,
-        bytes32 docId,
-        uint id,
+        uint256 docId,
+        uint8 singId,
         bytes16 signType,
         bytes sign
     );
 
     address owner;
 
-    mapping (bytes32 => Document) documents;
-    uint documentsCount;
-    mapping (uint => bytes32) ids;
+    uint256 documentsCount;
+    mapping (uint256 => Document) documents;
+
+    mapping (uint256 => uint256) ids;
 
     struct Document {
         address organizer;
@@ -33,53 +34,54 @@ contract SimpleSign {
         owner = msg.sender;
     }
 
-    function createDocument(uint nonce) returns (bytes32 docId) {
+    function createDocument(uint256 nonce) returns (uint256 docId) {
         docId = generateId(nonce);
         if (documents[docId].organizer != 0) throw;
-        uint index = documentsCount++;
+        uint256 index = documentsCount++;
         ids[index] = docId;
         documents[docId].organizer = msg.sender;
         Created(msg.sender, docId);
     }
 
-    function addSignature(bytes32 docId, bytes16 _type, bytes _sign) {
+    function addSignature(uint256 docId, bytes16 _type, bytes _sign) {
         Document doc = documents[docId];
         if (doc.organizer != msg.sender) throw;
+        if (doc.signs.length >= 0xFF) throw;
         uint idx = doc.signs.push(Sign(msg.sender, _type, _sign));
-        Signed(msg.sender, docId, idx, _type, _sign);
+        Signed(msg.sender, docId, uint8(idx), _type, _sign);
     }
 
     function getDocumentsCount() returns (uint) {
         return documentsCount;
     }
 
-    function getDocumentDetails(bytes32 docId) returns (address organizer, uint count) {
+    function getDocumentDetails(uint256 docId) returns (address organizer, uint count) {
         Document doc = documents[docId];
         organizer = doc.organizer;
         count = doc.signs.length;
     }
 
-    function getSignsCount(bytes32 docId) returns (uint) {
+    function getSignsCount(uint256 docId) returns (uint) {
         return documents[docId].signs.length;
     }
 
-    function getSignDetails(bytes32 docId, uint signId) returns (address, bytes16) {
+    function getSignDetails(uint256 docId, uint8 signId) returns (address, bytes16) {
         Document doc = documents[docId];
         Sign s = doc.signs[signId];
         return (s.signer, s.signType);
     }
 
-    function getSignData(bytes32 docId, uint signId) returns (bytes) {
+    function getSignData(uint256 docId, uint8 signId) returns (bytes) {
         Document doc = documents[docId];
         Sign s = doc.signs[signId];
         return s.sign;
     }
 
-    function generateId(uint nonce) returns (bytes32) {
-        return sha3(msg.sender, nonce);
+    function generateId(uint256 nonce) returns (uint256) {
+        return uint256(sha3(msg.sender, nonce));
     }
 
-    function getIdAtIndex(uint index) returns (bytes32) {
+    function getIdAtIndex(uint256 index) returns (uint256) {
         return ids[index];
     }
 
