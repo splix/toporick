fd27eb6e94260bb30a4f76d3181a2ac7cc0c1097
Igor Artamonov <igor@artamonov.ru>
2016-07-03 14:39:40 +0600

    better navigation on main screen

--- app/javascripts/components/startScreen.jsx
+++ app/javascripts/components/startScreen.jsx
@@ -8,10 +8,14 @@ const Render = ({onCreate}) => {
 
     return (
         <div className="row">
-            <div className="col-sm-10 col-sm-offset-1">
-                <p>To view details of an existing document, click on a document from list bellow (if there any).</p>
-                <p>Or create a new document:
-                    <button className="btn btn-primary" onClick={onCreate}><i className="fa fa-plus"/> Create New Document</button></p>
+            <div className="col-sm-5 col-sm-offset-1">
+                <h3>See latest documents</h3>
+                <p><i className="fa fa-2x fa-arrow-down direction"/>
+                    To view details of an existing document, click on a document from list bellow (if there any)</p>
+            </div>
+            <div className="col-sm-5">
+                <h3>Create a new document</h3>
+                <button className="btn btn-primary" onClick={onCreate}><i className="fa fa-plus"/> Create New Document</button>
             </div>
         </div>
     )
